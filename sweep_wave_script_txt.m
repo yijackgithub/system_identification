0f85be5737a07164b70bbb65d2e140af8022a6f6
Jack Yi <34209230+yigithub1988@users.noreply.github.com>
2018-01-06 10:43:11 +0800

    Matlab code

---
+++ sweep_wave_script_txt.m
@@ -0,0 +1,34 @@
+% This file is used to generate the input data for system identification.
+clear; clc; close all;
+%% Generate the sweep wave.
+f_sample = 500; % Sampling frequency is 500Hz
+Time = 50;
+N = f_sample*Time;% sweep wave sample numbers
+t = 0:1/f_sample:(N-1)/f_sample;% sweep wave time instants
+ampl = 1000; % the amplitude of the sweep wave
+sweep_wave = ampl.*chirp(t,0,50,10,'linear',-90); % generate 0-10Hz sweep wave
+sweep_wave = sweep_wave'; % tranpose the array
+sweep_wave_str = num2str(sweep_wave);% convert number to string
+
+for i = 1:f_sample*Time
+    sweep_wave_cell{i} = {sweep_wave_str(i,1:12)};% convert the string to the cell array
+end
+for i = 1:f_sample*Time
+    sweep_wave_str1(i) = strcat(sweep_wave_cell{i},',');% add the comma between the array elements
+end
+%% Plot time-series data and frequency-domain data.
+format short;
+f = linspace(0,f_sample,N);
+figure(1);
+plot(t(1:N), sweep_wave(1:N)); % display the time-series data
+xlim([0 100]);
+figure(2);
+sweep_wave_fft = fft(sweep_wave);
+plot(f,abs(sweep_wave_fft(1:N)));% display the frequency-domain data
+xlim([0 20]);
+
+%% Write the sweep_wave to txt file.
+filename = 'sweep_wave_500_50s_0_10Hz.txt';
+dlmwrite(filename,sweep_wave_str1,'delimiter','','precision',3);
+
+
